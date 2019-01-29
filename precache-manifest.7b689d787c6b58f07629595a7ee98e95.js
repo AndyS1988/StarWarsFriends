8bd523e72eedc0961da47b258f73a52f5667f9c0
AndyS1988 <spaska.andrea@gmail.com>
2019-01-28 18:32:04 -0800

    Updates

---
+++ precache-manifest.7b689d787c6b58f07629595a7ee98e95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7b66be977be383e0bc4",
+    "url": "/StarWarsFriends/static/js/runtime~main.d7b66be9.js"
+  },
+  {
+    "revision": "02509dbbb257217e46c6",
+    "url": "/StarWarsFriends/static/js/main.02509dbb.chunk.js"
+  },
+  {
+    "revision": "80d1d857f01b6b285c01",
+    "url": "/StarWarsFriends/static/js/1.80d1d857.chunk.js"
+  },
+  {
+    "revision": "02509dbbb257217e46c6",
+    "url": "/StarWarsFriends/static/css/main.d291414a.chunk.css"
+  },
+  {
+    "revision": "da669ec43d9f860422c0464b5296e731",
+    "url": "/StarWarsFriends/index.html"
+  }
+];
